379f36859257fd08e662cb8cb58c18c61c7610e5
Mohan Chouksey <162291835+MohanChoukse@users.noreply.github.com>
2026-07-11 00:22:08 +0530

    Time: 7 ms (32.47%), Space: 46.7 MB (9.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,21 +3,21 @@ class Solution {
         s.toLowerCase();
         t.toLowerCase();
 
-        if(s.length() == t.length()){
+        if (s.length() == t.length()) {
             char arr1[] = s.toCharArray();
             char arr2[] = t.toCharArray();
 
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
 
-    boolean str = Arrays.equals(arr1 , arr2);
-    if(str){
-        return true;
-    }else{
-        return false;
-    }
+            boolean str = Arrays.equals(arr1, arr2);
+            if (str) {
+                return true;
+            } else {
+                return false;
+            }
 
-        }else{
+        } else {
             return false;
         }
     }
