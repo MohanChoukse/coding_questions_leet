08c895e883105f8be0b25a157011e40c6de59b51
Mohan Chouksey <162291835+MohanChoukse@users.noreply.github.com>
2026-07-13 16:04:26 +0530

    Time: 13 ms (31.22%), Space: 52.6 MB (85.61%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,43 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int count = 0;
-        int candidate = 0;
+        // int count = 0;
+        // int candidate = 0;
 
-        for (int num : nums) {
-            if (count == 0) {
-                candidate = num;
-            }
-            if (num == candidate) {
-                count += 1;
-            } else {
-                count -= 1;
-            }
+        // for (int num : nums) {
+        //     if (count == 0) {
+        //         candidate = num;
+        //     }
+        //     if (num == candidate) {
+        //         count += 1;
+        //     } else {
+        //         count -= 1;
+        //     }
+        // }
+
+        // return candidate;
+
+
+
+
+        // HashMap Code
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            // if(map.containsKey(nums[i])){
+            //     map.put(nums[i], map.get(nums[i]) + 1);
+            // }else{
+            //     map.put(nums[i], 1);
+            // }
+
+            map.put(nums[i], map.getOrDefault(nums[i], 0) +1);
         }
 
-        return candidate;
+        // Set<Integer> keySet =  map.keySet();
+        for(Integer key : map.keySet() ){
+            if(map.get(key) > nums.length/2){
+                return key;
+            }
+        }
+        return -1;
     }
 }
